e99b431eea89956a84343fac6bb98a74a05930eb
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-30 13:33:16 -0700

    created get note-by-id and update-note queries

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,19 +2,48 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
-knex
-  .select('notes.id', 'title', 'content')
-  .from('notes')
-  .modify(queryBuilder => {
-    if (searchTerm) {
-      queryBuilder.where('title', 'like', `%${searchTerm}%`);
-    }
-  })
-  .orderBy('notes.id')
+// let searchTerm = 'Water';
+// knex
+//   .select('notes.id', 'title', 'content')
+//   .from('notes')
+//   .modify(queryBuilder => {
+//     if (searchTerm) {
+//       queryBuilder.where('title', 'like', `%${searchTerm}%`);
+//     }
+//   })
+//   .orderBy('notes.id')
+//   .then(results => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+/* 
+let noteId = 4;
+knex('notes')
+  .select()
+  .where('id', `${noteId}`)
   .then(results => {
-    console.log(JSON.stringify(results, null, 2));
+    console.log(JSON.stringify(results[0], null, 2));
   })
   .catch(err => {
     console.error(err);
   });
+ */
+
+let updatedNoteId = 3;
+let updatedObj = {
+  title: 'Another note about nothing', 
+  content: 'This is Another note about nothing'
+};
+knex('notes')
+  .where('id', updatedNoteId)
+  .update(updatedObj)
+  .returning(['title', 'content'])
+  .then(results => {
+    console.log(JSON.stringify(results[0], null, 2));
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
