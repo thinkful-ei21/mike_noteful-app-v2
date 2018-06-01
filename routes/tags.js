c2d9bbb1c9761042647a49e56712ff13119894a4
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-01 12:04:50 -0700

    created POST route for tags

--- routes/tags.js
+++ routes/tags.js
@@ -36,4 +36,28 @@ router.get('/:id', (req,res,next) => {
     .catch( err => next(err));
 });
 
+router.post('/', (req,res,next) => {
+  const { name } = req.body;
+  const newTag = { name };
+  const originalUrl = `http://${req.headers.host}/notes/${newTag.id}`;
+
+  // validate input
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  knex
+    .insert(newTag)
+    .into('tags')
+    .debug(true)
+    .returning(['id', 'name'])
+    .then((results) => {
+      const result = results[0];
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
 module.exports = router;
